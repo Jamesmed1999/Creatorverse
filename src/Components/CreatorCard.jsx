aa3a7f9f618d0be2a47f856a2ef38a29b3f9df69
Jamesmed1999 <74335705+Jamesmed1999@users.noreply.github.com>
2023-08-13 20:00:31 -0400

    re-did the css with picocss

--- src/Components/CreatorCard.jsx
+++ src/Components/CreatorCard.jsx
@@ -1,23 +1,32 @@
 import React from "react";
-import "./creator.css"
+//import "./creator.css"
 import { Link} from "react-router-dom";
 export const CreatorCard = ({name, description, imgLink, url, id, Slink}) => {
     
 
 
     return (<div className="creator-container">
-        <img src={imgLink} alt="creator img" className="creator-img"></img>
+      <main class="container">
+        <center>
+        <article className="creator-article">
+        <img src={imgLink} alt="creator img" className="creator-img"></img> 
         <h1 className="creator-name">{name}</h1>
-        <p className="creator-about">{description}</p>
-        <a href={Slink}>Check Me Out!</a>
+        <body><p className="creator-about">{description}</p>
+        
+        </body>
+       
         
         <Link
             style={{ display: "block", margin: "1rem 0" }}
             to={`/view/${url}`}
             key={id}
-          > <button className="creator-button">View</button>
+          > <button className="creator-button" role="button"  >View</button>
             </Link>
+            <a href={Slink}>Social Link:  {" " + Slink}</a>
+            </article>
+            </center>
             
+        </main>
          
         
         
